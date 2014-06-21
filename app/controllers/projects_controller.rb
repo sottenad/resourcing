6b9cf411dd15bebc79cc1c5d4c9ef770bc9553bc
Steve Ottenad <sottenad@gmail.com>
2014-06-21 10:12:31 -0700

    navigation styling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :ensure_valid_subscription
+  #before_filter :ensure_valid_subscription
   
   def index
   	@projects = Project.all
